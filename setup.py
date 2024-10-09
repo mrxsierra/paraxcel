b7ac9d02c9c98b2bc356229f4b65a44e22bec633
Sunil Sharma <9.sunilsharma@gmail.com>
2024-10-10 03:31:00 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=[
-        "python_docx",  # Add other dependencies your project may have
+        "python-docx",  # Add other dependencies your project may have
         "pandas",
         "openpyxl",
         "pydantic",
